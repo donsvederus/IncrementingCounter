3b6731263c95ee9d44d22c2bd007761cbe67a1cc
Don Svederus <svederus@gmail.com>
2022-05-25 23:42:57 -0600

    0.48.1 js script to increment numbers at the same time

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const counters = document.querySelectorAll('.counter')
 
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      // + sign make the string into a number.
+      const c = +counter.innerText;
+
+      const increment = target / 400
+
+      if(c < target) {
+          counter.innerText = `${Math.ceil(c + increment)}`
+          setTimeout(updateCounter, 1)
+      } else {
+          counter.innerText = target
+      }
+    }
+
+    updateCounter()
+})
